92275999c70c4e8e3210f463032fb038c91a7c0b
JB Damiano <Jean-Bernard.damiano@airnity.com>
2022-02-04 07:36:47 +0100

    rework build to add dependency

--- lib/mix/tasks/compile.dia.ex
+++ lib/mix/tasks/compile.dia.ex
@@ -43,39 +43,134 @@ defmodule Mix.Tasks.Compile.Dia do
   def run(_args) do
     project      = Mix.Project.config
     erlang_compile_opts = project[:dia_erl_compile_opts] || []
-    source_paths = project[:erlc_paths]
-    mappings     = Enum.zip(["dia"], source_paths)
     options      = project[:dia_options] || []
 
-    Erlang.compile(manifest(), mappings, :dia, :erl, erlang_compile_opts, fn
-      input, output ->
-        :ok = Filelib.ensure_dir(output)
-        app_path = Mix.Project.app_path(project)
-        include_path = to_charlist Path.join(app_path, project[:erlc_include_path])
-        :ok = Path.join(include_path, "dummy.hrl") |> Filelib.ensure_dir
-        case DiaDictUtil.parse({:path, input}, []) do
-          {:ok, spec} ->
-            filename = dia_filename(input, spec)
-            _ = DiaCodegen.from_dict(filename, spec, [{:outdir, 'src'} | options], :erl)
-            _ = DiaCodegen.from_dict(filename, spec, [{:outdir, include_path} | options], :hrl)
-            file = to_charlist(Path.join("src", filename))
-            compile_path = to_charlist Mix.Project.compile_path(project)
-            erlc_options = project[:erlc_options] || []
-            erlc_options = erlc_options ++ [{:outdir, compile_path}, {:i, include_path}, :report]
-            case :compile.file(file, erlc_options) do
-              {:ok, module} ->
-                {:ok, module, []}
-              {:ok, module, warnings} ->
-                {:ok, module, warnings}
-              {:ok, module, _binary, warnings} ->
-                {:ok, module, warnings}
-              {:error, errors, warnings} ->
-                {:error, errors, warnings}
-              :error ->
-                {:error, [], []}
-            end
-          error -> Mix.raise "Diameter compiler error: #{inspect error}"
+    dia_path = Path.join(Path.expand("."), "dia/")
+    files = Mix.Utils.extract_files([dia_path], "*.dia")
+
+
+
+
+    order = case compile_order(files) do
+      {:error, reason} -> Mix.raise "cannot retrive file order: #{inspect reason}"
+      {:ok, order} -> order
+    end
+
+    order |> Enum.each(fn input ->
+      output = Path.join(Path.expand("."), "src/")
+      :ok = Filelib.ensure_dir(output)
+      app_path = Mix.Project.app_path(project)
+      include_path = to_charlist Path.join(app_path, project[:erlc_include_path])
+      :ok = Path.join(include_path, "dummy.hrl") |> Filelib.ensure_dir
+      compile_path = to_charlist Mix.Project.compile_path(project)
+      environment = Mix.env()
+      int_erlc_option = if environment == :prod do
+        []
+      else
+        [:debug_info]
+      end
+
+      case DiaDictUtil.parse({:path, input}, [{:include, compile_path}]) do
+
+        {:ok, spec} ->
+          filename = dia_filename(input, spec)
+          _ = DiaCodegen.from_dict(filename, spec, [{:outdir, 'src'} | options], :erl)
+          _ = DiaCodegen.from_dict(filename, spec, [{:outdir, include_path} | options], :hrl)
+          file = to_charlist(Path.join("src", filename))
+
+          erlc_options = project[:erlc_options] || int_erlc_option
+          erlc_options = erlc_options ++ [{:outdir, compile_path}, {:i, include_path}, :report, :debug_info] ++ erlang_compile_opts
+
+          case :compile.file(file, erlc_options) do
+            {:ok, module} ->
+              {:ok, module, []}
+            {:ok, module, warnings} ->
+              {:ok, module, warnings}
+            {:ok, module, _binary, warnings} ->
+              {:ok, module, warnings}
+            {:error, errors, warnings} ->
+              {:error, errors, warnings}
+            :error ->
+              {:error, [], []}
+          end
+        error -> Mix.raise "Diameter compiler error: #{inspect error}"
+      end
+    end)
+  end
+
+
+
+  defp compile_order(diafiles) do
+    graph = :digraph.new()
+
+    diamods = diafiles |> Enum.map(fn f ->
+      dict = f |> Path.basename() |> Path.rootname()
+      {:"#{dict}", f}
+    end) |> Enum.into(%{})
+
+    diamods |> Enum.each(fn ({dict, f}) ->
+      case File.read(f) do
+        {:ok, bin} ->
+          inherits0 = String.split(bin, ["\n", "\r"], trim: true)
+          inherits1 = inherits0 |> Enum.map(fn x -> String.split(x, [" ", "\t"], trim: true) || x end)
+
+          inherits = inherits1 |> Enum.filter( fn x ->
+                                                  if Enum.at(x, 0) == "@inherits" do
+                                                    true
+                                                  else
+                                                    false
+                                                  end
+                                                end) |> Enum.map(fn x-> Enum.at(x, 1) end)
+          add(graph, {dict, inherits})
+
+        {:error,_reason} -> :ok
+      end
+    end)
+
+    order = case :digraph_utils.topsort(graph) do
+      false ->
+        case :digraph_utils.is_acyclic(graph) do
+          true -> {:error, :no_sort}
+          false -> [:error, :cycle]
         end
+      v ->
+        v |> List.foldl([], fn x, acc ->
+          key = case x do
+            n when is_atom(n) -> n
+            n -> :erlang.list_to_atom(n)
+          end
+
+          case Map.get(diamods,key, :undefined) do
+            :undefined -> acc
+            [file] -> [file |acc]
+            file -> [file |acc]
+          end
+        end)
+    end
+    :true = :digraph.delete(graph)
+    {:ok, order}
+
+  end
+
+
+  defp add(graph, {pkgname, deps}) do
+
+
+    v = case :digraph.vertex(graph, pkgname) do
+      false ->
+        :digraph.add_vertex(graph, pkgname)
+      {v, _} -> v
+    end
+
+    deps |> Enum.each(fn name1 ->
+      name = String.to_charlist(name1)
+
+      v3 = case :digraph.vertex(graph, name) do
+          false ->
+            :digraph.add_vertex(graph, name)
+          {v2, _} -> v2
+      end
+      :digraph.add_edge(graph, v, v3)
     end)
   end
 
